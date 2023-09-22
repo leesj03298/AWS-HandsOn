a58f1244831e431ce5958333639c79c5c1097ed8
lsj <leesj03298@naver.com>
2023-09-22 21:14:20 +0900

    Modify RouteTable Optimize

--- Redshift/Hands-On01/module/vpc/02-optimize.tf
+++ Redshift/Hands-On01/module/vpc/02-optimize.tf
@@ -4,11 +4,11 @@ locals {
                             ] if alltrue([length(aws_vpc.default) != 0, length(vpc.subnets) != 0])
                         ])
     route_tables    = flatten([ for vpc in var.vpc : distinct([ 
-                                    for subnet in vpc.subnets : merge(
-                                        {"route_table_name" = subnet.route_table_name },
-                                        {"vpc_name" = vpc.vpc_name },
-                                        {"tags" = vpc.tags } 
-                                    ) if subnet.route_table_name != null
+                                    for subnet in vpc.subnets : {
+                                        "route_table_name"  = subnet.route_table_name
+                                        "vpc_name"          = vpc.vpc_name
+                                        "tags"              = vpc.tags 
+                                    } if subnet.route_table_name != null
                             ]) if alltrue([length(aws_vpc.default) != 0, length(vpc.subnets) != 0])
                         ])
 }
